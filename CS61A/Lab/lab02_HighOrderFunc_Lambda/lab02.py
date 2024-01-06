341f67854ce1394e4e5289fb022655e29b6d442c
Auk <quinnxu19@foxmail.com>
2024-01-06 22:19:29 +0800

    update Q7 & Q8

--- CS61A/Lab/lab02_HighOrderFunc_Lambda/lab02.py
+++ CS61A/Lab/lab02_HighOrderFunc_Lambda/lab02.py
@@ -20,6 +20,7 @@ def lambda_curry2(func):
     "*** YOUR CODE HERE ***"
     # return lambda op: lambda x: lambda y: op(x, y)
     return lambda x: lambda y: func(x, y)
+
 # test
 # from operator import add, mul, mod
 # print(lambda_curry2(add)(3)(5))
@@ -59,6 +60,7 @@ def count_cond(condition):
     """
     "*** YOUR CODE HERE ***"
     return lambda n: sum([condition(n, i) for i in range(1, n+1)])
+
 # test
 # count_factors = count_cond(lambda n, i: n % i == 0)
 # print(count_factors(2))
@@ -73,7 +75,9 @@ def count_cond(condition):
 # print(count_primes(20))
 
 
-
+"""
+Q7: Composite Identity Function
+"""
 def compose1(f, g):
     """Return the composition function which given x, computes f(g(x)).
 
@@ -106,6 +110,16 @@ def composite_identity(f, g):
     False
     """
     "*** YOUR CODE HERE ***"
+    fg = compose1(f, g)
+    gf = compose1(g, f)
+    return lambda x: fg(x) == gf(x)
+
+# test
+# add_one = lambda x: x + 1
+# square = lambda x: x**2
+# b1 = composite_identity(square, add_one)
+# print(b1(0))
+# print(b1(4))
 
 
 
@@ -136,4 +150,37 @@ def cycle(f1, f2, f3):
     19
     """
     "*** YOUR CODE HERE ***"
+    def cycle_with_n(n):
+        resf = lambda x: x
+        p = n // 3
+        q = n % 3
+        for i in range(p):
+            f = compose1(f3, compose1(f2, f1))
+            resf = compose1(f, resf)
+        if q == 0:
+            return resf
+        elif q == 1:
+            return compose1(f1, resf)
+        else:
+            return compose1(compose1(f2, f1), resf)
 
+    return cycle_with_n
+
+# test
+# def add1(x):
+#     return x + 1
+# def times2(x):
+#     return x * 2
+# def add3(x):
+#     return x + 3
+# my_cycle = cycle(add1, times2, add3)
+# identity = my_cycle(0)
+# print(identity(5))
+# add_one_then_double = my_cycle(2)
+# print(add_one_then_double(1))
+# do_all_functions = my_cycle(3)
+# print(do_all_functions(2))
+# do_more_than_a_cycle = my_cycle(4)
+# print(do_more_than_a_cycle(2))
+# do_two_cycles = my_cycle(6)
+# print(do_two_cycles(1))
